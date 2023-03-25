bf9fc795df841ac3844d4f4a2fdfc3cced5d1ebf
Crowdbotics <team@crowdbotics.com>
2023-03-25 06:50:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jerry-manga-39539",
+  url: "https://jerry-manga-39539.botics.co",
+  api: "https://jerry-manga-39539.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
